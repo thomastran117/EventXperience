7ed239a9e515e97e6f8a56258281e80c69143ddc
thomastran117 <thomas.tran.1591@gmail.com>
2025-09-21 22:00:44 -0400

    EF Core - added one to many for club to club event

--- backend/DTO/ClubDTO.cs
+++ backend/DTO/ClubDTO.cs
@@ -37,12 +37,13 @@ public class ClubUpdateRequest
 
 public class ClubResponse
 {
-    public ClubResponse(int id, string name, string description, string clubtype)
+    public ClubResponse(int id, string name, string description, string clubtype, string clubimage)
     {
         Id = id;
         Name = name;
         Description = description;
         Clubtype = clubtype;
+        ClubImage = clubimage;
     }
 
     [Required]
@@ -56,6 +57,9 @@ public class ClubResponse
 
     [Required]
     public string Clubtype { get; set; } = string.Empty;
+
+    [Required]
+    public string ClubImage { get; set; } = string.Empty;
 
     public string? Phone { get; set; }
     public string? Email { get; set; }
--- backend/Model/Club.cs
+++ backend/Model/Club.cs
@@ -6,9 +6,12 @@ public class Club
     public required string Name { get; set; }
     public required string Description { get; set; }
     public required string Clubtype { get; set; }
+    public required string ClubImage { get; set; }
     public string? Phone { get; set; }
     public string? Email { get; set; }
     public double? Rating { get; set; }
     public int UserId { get; set; }
     public required User User { get; set; }
+    public ICollection<EventClub> EventClubs { get; set; } = new List<EventClub>();
+
 }

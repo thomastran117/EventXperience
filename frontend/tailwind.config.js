3a4671fd0eff3128355f0d33262f33872e76c31e
Thomas <88253986+thomastran117@users.noreply.github.com>
2025-12-03 07:04:58 -0500

    Add prettier (#30)
    * Adding components, skeletons and authservice
    
    * login work, auth module, and renamed to /features
    
    * Fixed cors
    
    * Login form
    
    * Added nice icons
    
    * Install prettier, format

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,28 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {
       animation: {
-        "gradient-slow": "gradient 8s ease infinite",
-        blob: "blob 9s infinite",
+        'gradient-slow': 'gradient 8s ease infinite',
+        blob: 'blob 9s infinite',
       },
       keyframes: {
         gradient: {
-          "0%, 100%": { backgroundPosition: "0% 50%" },
-          "50%": { backgroundPosition: "100% 50%" },
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
         },
         blob: {
-          "0%": { transform: "translate(0, 0) scale(1)" },
-          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
-          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
-          "100%": { transform: "translate(0, 0) scale(1)" },
+          '0%': { transform: 'translate(0, 0) scale(1)' },
+          '33%': { transform: 'translate(30px, -50px) scale(1.1)' },
+          '66%': { transform: 'translate(-20px, 20px) scale(0.9)' },
+          '100%': { transform: 'translate(0, 0) scale(1)' },
         },
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [],
 };

e6ee66e08b7f84599a10fe6535772be65f95dc95
thomastran117 <thomas.tran.1591@gmail.com>
2025-12-02 23:48:55 -0500

    Fixed cors

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,22 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { CoreModule } from './core/core.module';
+import { provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
+
+import { userReducer } from './core/stores/user.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideStore({ user: userReducer }),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    importProvidersFrom(CoreModule),
+  ],
 };
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SessionManagerService } from './core/services/session-manager.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.html',
-  styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+  private session = inject(SessionManagerService);
+  protected readonly title = 'frontend';
+
+  ngOnInit(): void {
+    this.session.restoreSession();
+  }
+
+  get loading() {
+    return this.session.loading();
+  }
 }

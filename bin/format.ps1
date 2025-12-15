14179beae42ea5f73d4a5b0a574a906c1f76d183
thomastran117 <thomas.tran.1591@gmail.com>
2025-12-15 00:58:34 -0500

    bin format script

---
+++ bin/format.ps1
@@ -0,0 +1,39 @@
+$root = Get-Location
+Set-Location -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
+
+Write-Host "Running format script for backend and frontend..." -ForegroundColor Cyan
+
+function Prettier-Format {
+    param (
+        [string]$path
+    )
+
+    Write-Host ""
+    Write-Host "Formatting in: $path" -ForegroundColor Yellow
+
+    Push-Location $path
+    npm run format
+    Pop-Location
+}
+
+function Dotnet-Format {
+    param (
+        [string]$path
+    )
+
+    Write-Host ""
+    Write-Host "Formatting in: $path" -ForegroundColor Yellow
+
+    Push-Location $path
+    dotnet format
+    Pop-Location
+}
+
+Dotnet-Format "..\backend"
+Prettier-Format "..\frontend"
+Dotnet-Format "..\worker"
+
+Write-Host ""
+Write-Host "All formatting complete." -ForegroundColor Cyan
+
+Set-Location $root

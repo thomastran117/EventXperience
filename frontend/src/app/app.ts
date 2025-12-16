32e51a87ab1be0adac34cb8604d678f4bf7226bb
Thomas <88253986+thomastran117@users.noreply.github.com>
2025-12-15 22:50:40 -0500

    added footer component (#85)

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SessionManagerService } from './core/services/session-manager.service';
 import { NgIf } from '@angular/common';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NavbarComponent],
+  imports: [RouterOutlet, NgIf, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
 })
 export class App implements OnInit {

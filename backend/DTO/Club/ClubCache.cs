a2a562ec8c914b622feea32b1f7af7a050fb5354
thomastran117 <thomas.tran.1591@gmail.com>
2025-12-16 22:02:27 -0500

    Updated club service to use cache better

---
+++ backend/DTO/Club/ClubCache.cs
@@ -0,0 +1,15 @@
+namespace backend.DTOs
+{
+    public record ClubCacheDto(
+        int Id,
+        string Name,
+        string Description,
+        string Clubtype,
+        string ClubImage,
+        string? Phone,
+        string? Email,
+        int MemberCount,
+        bool IsVerified,
+        int UserId
+    );
+}

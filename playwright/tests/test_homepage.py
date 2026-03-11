38c32f820f7c3f7b7dd1763a86212a40cb87cd60
thomastran117 <thomas.tran.1591@gmail.com>
2026-03-10 22:12:44 -0400

    Removed playwright

--- playwright/tests/test_homepage.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from playwright.async_api import expect
-
-@pytest.mark.asyncio(loop_scope="session")
-async def test_homepage(goto, page):
-    await goto("/")
-    await expect(page).to_have_url("http://localhost:3040/")
